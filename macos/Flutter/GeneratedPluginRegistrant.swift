71614d6eb76c6e1bd8af528ef3ee6e9c540547bc
topfany <1228131023@qq.com>
2026-04-24 12:29:39 +0800

    Add JNI plugin, remove macOS PathProvider
    Add the 'jni' entry to the FLUTTER_FFI_PLUGIN_LIST for Linux and Windows, remove the path_provider_foundation import and PathProvider registration on macOS, and relax SDK constraints in pubspec.yaml. Also update dependency state in pubspec.lock (mirror, SDK constraints and dependency bumps) to align with the new environment constraints.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import app_links
 import file_picker
 import package_info_plus
-import path_provider_foundation
 import sqlite3_flutter_libs
 import url_launcher_macos
 import vpn_plugin
@@ -17,7 +16,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   VpnPlugin.register(with: registry.registrar(forPlugin: "VpnPlugin"))
